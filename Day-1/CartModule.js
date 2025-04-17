1bd48cccbf174f89e94dbb8d85f4edfa46056e25
Nipesh <nipeshincbca2021@gmail.com>
2025-04-17 12:10:15 +0545

    Day 1 commit

---
+++ Day-1/CartModule.js
@@ -0,0 +1,10 @@
+let addToCart = ()=>{
+    return "Add to Cart"
+}
+
+let Num =()=>{
+    return 5
+}
+
+//module.exports= addToCart
+module.exports = { addToCart, Num}
---
+++ Day-1/index.js
@@ -0,0 +1,16 @@
+const {addToCart,Num} = require("./CartModule");
+
+const a=2
+console.log("Hello")
+console.log(a)
+
+let x =10
+for(i=1; i<10; i++){
+    console.log(i);
+}
+console.log("Helloo")
+
+
+addToCart
+console.log(addToCart())
+console.log(Num())
